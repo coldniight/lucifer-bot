eb2548bdda401397de353c7863edfb597445d021
coldniight <49598352+coldniight@users.noreply.github.com>
2019-05-12 16:54:26 -0500

    Update guildMemberAdd.js

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,6 +1,6 @@
 const { prefix } = require("../../botconfig.json")
 
 module.exports = async (bot, member) => {
-    var role = bot.guilds.get("574756014163886111").roles.find('name', 'Unverified');
+    var role = bot.guilds.get("577220184692097034").roles.find('name', 'Unverified');
     member.addRole(role);
 }
