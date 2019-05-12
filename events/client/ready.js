4d49111dec60b2cf32a81784cc63b2cc824d754b
coldniight <49598352+coldniight@users.noreply.github.com>
2019-05-12 16:54:45 -0500

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,5 +3,4 @@ const { prefix } = require("../../botconfig.json")
 module.exports = async (bot) => {
     bot.user.setActivity('over the server', {type: 'WATCHING'});
     bot.user.setStatus('dnd');
-    bot.guilds.get("574756014163886111").channels.get("574763279717629963").send("<@&574765872967843841> Bot updated.")
 }
